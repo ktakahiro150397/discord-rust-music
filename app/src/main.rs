7f2bbb04e1d8fa5f716d466dcde689847ccabcf7
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-08-24 10:26:26 +0900

    テスト用DL処理確認

--- app/src/main.rs
+++ app/src/main.rs
@@ -11,38 +11,69 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[tokio::main]
 async fn main() {
-    dotenv().expect(".env file not found!");
+    // Launch
+    Straylight::run_test().await;
+}
 
-    let token = std::env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not found in environment");
-    let intents = serenity::GatewayIntents::non_privileged();
+struct Straylight {}
 
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands: vec![
-                commands::test::age(),
-                commands::test::test(),
-                commands::test::download(),
-                commands::test::playlist(),
-                commands::utility::register(),
-                commands::utility::help(),
-            ],
-            prefix_options: poise::PrefixFrameworkOptions {
-                prefix: Some("::".to_string()),
+impl Straylight {
+    async fn run_test() {
+        let temp_path = std::path::PathBuf::from("temp");
+        let mut playlist = playlist::playlist::PlayList::new();
+
+        let track = match playlist::track::Track::from_youtube_url(
+            &temp_path,
+            "https://www.youtube.com/watch?v=pPoIneB_KLI&list=RDpPoIneB_KLI",
+        )
+        .await
+        {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Error: {}", e);
+                return;
+            }
+        };
+
+        playlist.add(track);
+
+        dbg!(playlist);
+    }
+
+    async fn run() {
+        dotenv().expect(".env file not found!");
+
+        let token = std::env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not found in environment");
+        let intents = serenity::GatewayIntents::non_privileged();
+
+        let framework = poise::Framework::builder()
+            .options(poise::FrameworkOptions {
+                commands: vec![
+                    commands::test::age(),
+                    commands::test::test(),
+                    commands::test::download(),
+                    commands::test::playlist(),
+                    commands::utility::register(),
+                    commands::utility::help(),
+                ],
+                prefix_options: poise::PrefixFrameworkOptions {
+                    prefix: Some("::".to_string()),
+                    ..Default::default()
+                },
                 ..Default::default()
-            },
-            ..Default::default()
-        })
-        .setup(|ctx, _ready, framework| {
-            Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
             })
-        })
-        .build();
+            .setup(|ctx, _ready, framework| {
+                Box::pin(async move {
+                    poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                    Ok(Data {})
+                })
+            })
+            .build();
 
-    let client = serenity::ClientBuilder::new(token, intents)
-        .framework(framework)
-        .await;
+        let client = serenity::ClientBuilder::new(token, intents)
+            .framework(framework)
+            .await;
 
-    client.unwrap().start().await.unwrap();
+        client.unwrap().start().await.unwrap();
+    }
 }
